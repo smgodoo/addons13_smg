9da137a2e8910d7004e1e7c767a7a059de370558
unknown <khanvuthy88@gmail.com>
2020-04-29 16:12:50 +0700

    Only assingned user can edit project

--- project_owner_team/models/models.py
+++ project_owner_team/models/models.py
@@ -6,15 +6,13 @@ from odoo import models, fields, api
 class Project(models.Model):
     _inherit = 'project.project'
 
-    current_user = fields.Many2one('res.users', compute="_compute_current_user")
-    current_user_id = fields.Integer(compute="_compute_current_user")
-    project_user_id = fields.Integer(compute="_compute_project_user")
+    can_edit_record = fields.Boolean(default=False, compute="_compute_project_user")
 
     def _compute_current_user(self):
         return self.env.user
 
     def _compute_project_user(self):
-        user = self.env['res.users'].search([('id', '=', self.env.user.id)], limit=1)
         for record in self:
-            record.current_user_id = user.id
-            record.project_user_id = record.user_id.id
+            if record.user_id.id == self.env.user.id:
+                record.can_edit_record = True
+            print(record.can_edit_record)
