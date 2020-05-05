e92f7a920789f2c97890fcf5e84c5fff3fd88afb
unknown <khanvuthy88@gmail.com>
2020-05-05 14:02:47 +0700

    Add fields : sub_category_id to approval.request, add parent_id field to approval.category

--- document_approval_filter/models/models.py
+++ document_approval_filter/models/models.py
@@ -6,4 +6,16 @@ from odoo import models, fields, api
 class ApprovalRequest(models.Model):
     _inherit = ['approval.request']
 
+    sub_category_id = fields.Many2one('approval.category', string='Sub Category')
     department_id = fields.Many2one('hr.department', string='Department', track_visibility='onchange')
+
+    @api.onchange('category_id')
+    def onchange_category_id(self):
+        self.sub_category_id = []
+        return {'domain': {'sub_category_id': [('parent_id', 'in', [self.category_id.id])]}}
+
+
+class ApprovalsCategory(models.Model):
+    _inherit = 'approval.category'
+
+    parent_id = fields.Many2one('approval.category')
