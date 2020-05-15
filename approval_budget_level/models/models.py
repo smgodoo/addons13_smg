31e80bdd1b058b83172415eb8b9f7e986e07d9e1
unknown <khanvuthy88@gmail.com>
2020-05-15 15:42:53 +0700

    Add on_hold action and status

--- approval_budget_level/models/models.py
+++ approval_budget_level/models/models.py
@@ -38,14 +38,59 @@ class ApprovalType(models.Model):
     budget_lines = fields.One2many(
         comodel_name="approval.budget",
         inverse_name="request_id", )
-    is_hod_approver = fields.Boolean(stirng="Head Of Department",
-                                     help="Automatically add the CEO as approver on the request.")
-    is_ceo_approver = fields.Boolean(stirng="CEO",
-                                     help="Automatically add the CEO as approver on the request.")
+    is_hod_approver = fields.Boolean(string="Head Of Department",
+                                     help="Automatically add Head Of Department as approver on the request.")
+    is_ceo_approver = fields.Boolean(string="CEO",
+                                     help="Automatically add CEO as approver on the request.")
 
 
 class ApprovalRequest(models.Model):
     _inherit = 'approval.request'
+
+    request_status = fields.Selection([
+        ('new', 'To Submit'),
+        ('pending', 'Submitted'),
+        ('approved', 'Approved'),
+        ('refused', 'Refused'),
+        ('on_hold', ' On-Hold'),
+        ('cancel', 'Cancel')], default="new", compute="_compute_request_status", store=True, compute_sudo=True,
+        group_expand='_read_group_request_status')
+    user_status = fields.Selection([
+        ('new', 'New'),
+        ('pending', 'To Approve'),
+        ('approved', 'Approved'),
+        ('refused', 'Refused'),
+        ('on_hold', ' On-Hold'),
+        ('cancel', 'Cancel')], compute="_compute_user_status")
+
+    @api.depends('approver_ids.status')
+    def _compute_user_status(self):
+        for approval in self:
+            approval.user_status = approval.approver_ids.filtered(
+                lambda approver: approver.user_id == self.env.user).status
+
+    @api.depends('approver_ids.status')
+    def _compute_request_status(self):
+        for request in self:
+            status_lst = request.mapped('approver_ids.status')
+            minimal_approver = request.approval_minimum if len(status_lst) >= request.approval_minimum else len(
+                status_lst)
+            if status_lst:
+                if status_lst.count('cancel'):
+                    status = 'cancel'
+                elif status_lst.count('refused'):
+                    status = 'refused'
+                elif status_lst.count('new'):
+                    status = 'new'
+                elif status_lst.count('on_hold'):
+                    status = 'on_hold'
+                elif status_lst.count('approved') >= minimal_approver:
+                    status = 'approved'
+                else:
+                    status = 'pending'
+            else:
+                status = 'new'
+            request.request_status = status
 
     @api.onchange('category_id', 'request_owner_id')
     def _onchange_category_id(self):
@@ -68,3 +113,23 @@ class ApprovalRequest(models.Model):
                 'user_id': user.id,
                 'request_id': self.id,
                 'status': 'new'})
+
+    def action_on_hold(self, approver=None):
+        if not isinstance(approver, models.BaseModel):
+            approver = self.mapped('approver_ids').filtered(
+                lambda approver: approver.user_id == self.env.user
+            )
+        approver.write({'status': 'on_hold'})
+        self.sudo()._get_user_approval_activities(user=self.env.user).action_feedback()
+
+
+class ApprovalApprover(models.Model):
+    _inherit = 'approval.approver'
+
+    status = fields.Selection([
+        ('new', 'New'),
+        ('pending', 'To Approve'),
+        ('approved', 'Approved'),
+        ('refused', 'Refused'),
+        ('on_hold', ' On-Hold'),
+        ('cancel', 'Cancel')], string="Status", default="new", readonly=True)
